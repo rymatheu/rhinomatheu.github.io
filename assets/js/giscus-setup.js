b357c053c25c13d198ce81d730f486779e565c1b
rymatheu <91397609+rymatheu@users.noreply.github.com>
2026-09-17 03:18:28 +0000

    Deploying to gh-pages from @ rymatheu/rhinomatheu.github.io@d76c1f4c61d6e57b22555af8b77d01fae27986c2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "rhinomatheu/rhinomatheu.github.io",
+    "data-repo": "rymatheu/rhinomatheu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
